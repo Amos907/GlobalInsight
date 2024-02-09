1f66c95af7d260f0e9cfefb04019b05d4d5f57bf
Amos907 <amosmaina907@gmail.com>
2024-02-09 07:05:23 +0300

    Initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +9,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        ...defaultTheme.colors,
+
+        primary: "#c10000",
+        secondary: "#EAEAEA",
       },
     },
   },
